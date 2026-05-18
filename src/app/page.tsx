ca48e7758fd879af8fe06febac838d211c1ccb18
usmanMTM <usman@themtmagency.com>
2026-05-18 19:36:32 +0100

    overflow hidden on mo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="flex-1">
+    <main className="flex-1 overflow-hidden">
       <Navbar />
       <Hero />
       <Skills />
